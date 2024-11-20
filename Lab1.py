be18ed5e6f8a6adcd64b7a12b44264834164daea
parrellag1 <parrellag1@southernct.edu>
2024-11-20 21:17:17 +0000

    Labs Complete

--- Lab1.py
+++ Lab1.py
@@ -5,15 +5,15 @@ def lab_1():
         score = float (input(f"enter score number {i+1}: "))
         scores_list.append(score)
         print(scores_list)
-        avg_score = sum(scores_list)/len(scores_list)
-        if avg_score >= 90:
-            print("A", avg_score)
-        elif avg_score >= 80 and avg_score < 90:
-            print("B" avg_score)
-        elif avg_score >= 70 and avg_score < 80:
-            print("C" avg_score)
-        elif avg_score >= 65 and avg_score < 70:
-            print("D" avg_score)
-        else:
-            print("F", avg_score)
-        lab_1()
+    avg_score = sum(scores_list)/len(scores_list)
+    if avg_score >= 90:
+        print("A", avg_score)
+    elif avg_score >= 80 and avg_score < 90:
+        print("B", avg_score)
+    elif avg_score >= 70 and avg_score < 80:
+        print("C", avg_score)
+    elif avg_score >= 65 and avg_score < 70:
+        print("D", avg_score)
+    else:
+        print("F", avg_score)
+lab_1()
---
+++ Lab2.py
@@ -0,0 +1,4 @@
+nums = input(int("enter numbers seperated by spaces: "))
+nums_list = nums.split(" ")
+for num in nums_list:
+    if nums 
